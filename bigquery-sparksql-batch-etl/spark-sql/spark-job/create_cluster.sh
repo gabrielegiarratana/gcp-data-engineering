515c567cb10b9430cb63f4c73d290fe6abd49c7c
Gabriele Giarratana <gabrielegiarratana000@gmail.com>
2024-05-29 19:39:05 +0200

    Adding create cluster script as generated by GCP UI

--- bigquery-sparksql-batch-etl/spark-sql/spark-job/create_cluster.sh
+++ bigquery-sparksql-batch-etl/spark-sql/spark-job/create_cluster.sh
@@ -1,10 +1,20 @@
-gcloud dataproc clusters create spark-dwh \
- --scopes=default \
- --region "us-east1" --zone "us-east1-b" \
- --initialization-actions=gs://dataproc-initialization-actions/jupyter/jupyter.sh \
- --master-machine-type n1-standard-2 \
- --master-boot-disk-size 200 \
-  --num-workers 2 \
---worker-machine-type n1-standard-2 \
---worker-boot-disk-size 200 \
---image-version 1.3
+gcloud dataproc clusters create cluster-gg \
+--scopes=default \
+--subnet default \
+--no-address \
+--region europe-west8 \
+--no-address \
+--zone europe-west8-a \
+--master-machine-type n2-standard-2 \
+--master-boot-disk-size 30 \
+--num-workers 2 \
+--enable-component-gateway \
+--worker-machine-type n2-standard-2 \
+--worker-boot-disk-size 30 \
+--image-version 2.2-debian12 \
+--optional-components JUPYTER,ZEPPELIN \
+--project prj-01-gg
+
+
+# Generated from GCP UI
+gcloud dataproc clusters create cluster-01-gg --enable-component-gateway --bucket jobcluster-gg --region europe-west8 --master-machine-type n2-standard-2 --master-boot-disk-type pd-balanced --master-boot-disk-size 30 --num-master-local-ssds 1 --master-local-ssd-interface NVME --num-workers 2 --worker-machine-type n2-standard-2 --worker-boot-disk-type pd-balanced --worker-boot-disk-size 30 --num-worker-local-ssds 1 --worker-local-ssd-interface NVME --image-version 2.2-debian12 --properties spark:spark.dataproc.enhanced.optimizer.enabled=true,spark:spark.dataproc.enhanced.execution.enabled=true,dataproc:dataproc.cluster.caching.enabled=true --optional-components JUPYTER,ZEPPELIN --labels dept=test --project prj-01-gg
